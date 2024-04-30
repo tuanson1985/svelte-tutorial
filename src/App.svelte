c2a3d4561783cc6a0b3159019e1628318d211f06
tuanson1985 <105621622+tuanson1985@users.noreply.github.com>
2024-04-30 14:52:19 +0700

    Form Handling (input, textarea, select, multiple select)

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,59 @@
 <script>
-  let count = 0;
-  function handleClick() {
-    count += 1;
-  }
-  function handleClickEvent(event, stepSize) {
-    count += stepSize;
-  }
+  const formValues = {
+    name: "",
+    profileSummary: "",
+    country: "",
+    jobLocation: [],
+  };
 </script>
 
 <main>
-  <button on:click={() => (count = count + 1)}>Count {count}</button>
-  <button on:click={handleClick}>Count {count}</button>
-  <button on:click={(event) => handleClickEvent(event, 5)}>Count {count}</button
-  >
+  <div>
+    <pre>
+		{JSON.stringify(formValues, null, 2)}
+	</pre>
+  </div>
+  <form>
+    <div>
+      <label for="name">Name</label>
+      <input type="text" id="name" bind:value={formValues.name} />
+    </div>
+
+    <div>
+      <label for="profile">Profile Summary</label>
+      <textarea name="" id="profile" bind:value={formValues.profileSummary} />
+    </div>
+
+    <div>
+      <label for="country">Country</label>
+      <select id="country" bind:value={formValues.country}>
+        <option value="">Select a country</option>
+        <option value="india">India</option>
+        <option value="vietname">Vietnam</option>
+        <option value="singapore">Singapore</option>
+      </select>
+    </div>
+
+    <div>
+      <label for="job-location">Job location</label>
+      <select id="job-location" bind:value={formValues.jobLocation} multiple>
+        <option value="">Select a country</option>
+        <option value="php">PHP</option>
+        <option value="java">Java</option>
+        <option value="typescript">TypeScript</option>
+      </select>
+    </div>
+
+	
+  </form>
 </main>
 
 <style>
+  input + label {
+    display: inline-block;
+  }
   main {
-    text-align: center;
+    /* text-align: center; */
     padding: 1em;
     max-width: 240px;
     margin: 0 auto;
