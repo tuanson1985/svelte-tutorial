58c7496c7758701f1ef659cf0277413ca2b1e1e6
tuanson1985 <105621622+tuanson1985@users.noreply.github.com>
2024-04-30 15:06:05 +0700

    Form Handling (checkbox, checkbox group, ratio,form submit)

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,14 @@
     profileSummary: "",
     country: "",
     jobLocation: [],
+    remoteWork: false,
+    skillSet: [],
+    yearsOfExperience: "",
   };
+
+  function submitForm() {
+    console.log("form values", formValues);
+  }
 </script>
 
 <main>
@@ -13,7 +20,7 @@
 		{JSON.stringify(formValues, null, 2)}
 	</pre>
   </div>
-  <form>
+  <form on:submit|preventDefault={submitForm}>
     <div>
       <label for="name">Name</label>
       <input type="text" id="name" bind:value={formValues.name} />
@@ -44,7 +51,75 @@
       </select>
     </div>
 
-	
+    <div>
+      <input
+        type="checkbox"
+        id="remoteWork"
+        bind:checked={formValues.remoteWork}
+      />
+      <label for="remoteWork">Open to remote work?</label>
+    </div>
+
+    <div>
+      <label>Skill set</label>
+      <input
+        type="checkbox"
+        id="html"
+        value="html"
+        bind:group={formValues.skillSet}
+      />
+      <label for="html">HTML</label>
+      <input
+        type="checkbox"
+        id="css"
+        value="css"
+        bind:group={formValues.skillSet}
+      />
+      <label for="css">CSS</label>
+      <input
+        type="checkbox"
+        id="javascript"
+        value="javascript"
+        bind:group={formValues.skillSet}
+      />
+      <label for="javascript">JavaScript</label>
+    </div>
+
+    <div>
+      <label>Years of Experience</label>
+      <input
+        type="radio"
+        id="0-2"
+        value="0-2"
+        bind:group={formValues.yearsOfExperience}
+      />
+      <label for="0-2">0-2</label>
+      <input
+        type="radio"
+        id="3-5"
+        value="3-5"
+        bind:group={formValues.yearsOfExperience}
+      />
+      <label for="3-5">3-5</label>
+      <input
+        type="radio"
+        id="6-10"
+        value="6-10"
+        bind:group={formValues.yearsOfExperience}
+      />
+      <label for="6-10">6-10</label>
+      <input
+        type="radio"
+        id="10+"
+        value="10+"
+        bind:group={formValues.yearsOfExperience}
+      />
+      <label for="10+">10+</label>
+    </div>
+
+    <div>
+      <button>Submit</button>
+    </div>
   </form>
 </main>
 
